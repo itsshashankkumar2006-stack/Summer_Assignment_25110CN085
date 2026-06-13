455660194597ed84d481b49a7e13e03a9b369eac
itsshashankkumar2006-stack <itsshashankkumar2006@gmail.com>
2026-06-14 01:14:50 +0530

    Added DAY13 programs

---
+++ DAY13/programs2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main() {
+    int arr[100], n, i, sum = 0;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+        sum = sum + arr[i];
+    }
+
+    float average= (float)sum / n;
+
+    printf("Sum: %d\n", sum);
+    printf("Average: %.2f\n", average);
+    return 0;
+}
